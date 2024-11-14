0b1da76599629f7e5bb460000b555f1ee5e04598
Maksim Ostroginskiy <ostroginskiymaksim@gmail.com>
2024-11-14 22:14:07 +0200

    All tasks were completed

--- Lab4/Exercises/6-matrix.js
+++ Lab4/Exercises/6-matrix.js
@@ -1,9 +1,16 @@
 'use strict';
 
 const max = (matrix) => {
-  // Use nested for loop to find max value in 2d matrix
-  // For example max([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-  // should return 9
+  let value = 0
+  for(let i = 0; i < matrix.length; i++) {
+    let row = matrix[i];
+    for(let j = 0; j < row.length; j++) {
+        if(value < row[j]) value = row[j]
+    }
+  }
+  return value
 };
 
+const m = max([[20, 2, 3], [4, 5, 6], [7, 8, 9]]);
+console.log(m); // 9
 module.exports = { max };
--- Lab4/Exercises/7-ages.js
+++ Lab4/Exercises/7-ages.js
@@ -1,19 +1,21 @@
 'use strict';
 
 const ages = (persons) => {
-  // Use for..in to calculate age for each person
-  // For example ages({
-  //   lenin: { born: 1870, died: 1924 },
-  //   mao: { born: 1893, died: 1976 },
-  //   gandhi: { born: 1869, died: 1948 },
-  //   hirohito: { born: 1901, died: 1989 },
-  // })
-  // should return {
-  //   lenin: 54,
-  //   mao: 83,
-  //   gandhi: 79,
-  //   hirohito: 88,
-  // }
+  const result = {};
+  
+  for (let name in persons) {
+    const { died, born } = persons[name]
+    result[name] = died - born
+  }
+
+  return result;
 };
 
+const persons = {
+  lenin: { born: 1870, died: 1924 },
+  mao: { born: 1893, died: 1976 },
+  gandhi: { born: 1869, died: 1948 },
+  hirohito: { born: 1901, died: 1989 },
+};
+console.log(ages(persons));
 module.exports = { ages };
