dae46ff3702712733487c4d1cf033a4bc23257ec
Maksim Ostroginskiy <ostroginskiymaksim@gmail.com>
2024-11-18 01:21:07 +0200

    Lab6 was completed

--- Lab6/Exercises/1-pipe.js
+++ Lab6/Exercises/1-pipe.js
@@ -1,5 +1,15 @@
 'use strict';
 
-const pipe = (...fns) => (x) => null;
+const pipe = (...fns) => {
+    for (const fn of fns) {
+        if(typeof fn !== 'function') {
+            throw Error('All compose arguments should be functions')
+        }
+    }
+    return (num) => fns.reduce((acc, func) => func(acc), num)
+}
+
+
+
 
 module.exports = { pipe };
